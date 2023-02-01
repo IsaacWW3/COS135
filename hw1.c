110ce220da86176fa7c03cadc469deec977ebf75
Isaac <112578560+IsaacWW3@users.noreply.github.com>
2023-02-01 11:38:30 -0500

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello, \nUmaine!\n");
+	printf("    *\n   **\n  ***\n ****\n*****\n");
+	printf("\n*****\n *** \n  *  \n");
+	printf("|\\ \n| \\ \n|  \\ \n|___\\ \n ");
+	printf("\n    *  *        *\n\n  *       *  *  \n\n*\n ");
+	return 0;
+}
